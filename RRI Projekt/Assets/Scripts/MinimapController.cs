955a360561eb86979780cb9e56a74e669ced5f4a
Hana Hrustić <hana.hrustic@gmail.com>
2022-01-20 00:58:36 +0100

    Added minimap and player health.

---
+++ RRI Projekt/Assets/Scripts/MinimapController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    public Transform child;
+
+    void LateUpdate()
+    {
+        child.transform.rotation = Quaternion.Euler(90, gameObject.transform.rotation.y * -1.0f, 0.0f);
+    }
+}
---
+++ RRI Projekt/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Text gameOverText;
+
+    bool isDead;
+
+    void Awake()
+    {
+        gameOverText.enabled = false;
+
+        currentHealth = startingHealth;
+        healthSlider.value = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        
+        if (currentHealth <= 0 && !isDead)
+        {
+            StartCoroutine(Death());
+        }
+    }
+
+    IEnumerator Death()
+    {
+        isDead = true;
+        gameOverText.enabled = true;
+
+        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX |
+            RigidbodyConstraints.FreezeRotationY |
+            RigidbodyConstraints.FreezeRotationZ |
+            RigidbodyConstraints.FreezePositionY |
+            RigidbodyConstraints.FreezePositionY |
+            RigidbodyConstraints.FreezePositionZ;
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
